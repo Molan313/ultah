f7030532ca369f9a0047647e210f95f35c9b356c
Molan313 <faisalmasrimaulana@gmail.com>
2024-09-28 13:48:26 +0700

    selesaii

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ document.getElementById('cek').addEventListener('click', function(event) {
     event.preventDefault();
 
     const nameInput = document.getElementById('name').value.toLowerCase().trim();
-
-    if (nameInput === "wina suresti") {
+    
+    if(!nameInput) {
+        alert("Nama tidak boleh kosong");
+    }
+    else if (nameInput === "wina suresti") {
         window.location.href = "ultah.html"; // Redirect to ultah.html
-    } else {
+    }
+    else if ("nameInput === faisal masri maulana") {
+        alert("Itu nama saya! (╬≖_≖)")
+    }
+    else {
         window.location.href = "salah.html"; // Redirect to no.html
     }
 });
